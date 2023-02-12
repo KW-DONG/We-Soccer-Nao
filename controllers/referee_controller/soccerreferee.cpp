9397d2064c56bf83c4d78c1396d80099c311825b
Kaiwen Dong <42641126+KW-DONG@users.noreply.github.com>
2023-02-12 02:04:13 +0000

    Define the states and events

--- controllers/referee_controller/soccerreferee.cpp
+++ controllers/referee_controller/soccerreferee.cpp
@@ -1,56 +1,163 @@
 #include "soccerreferee.h"
+#include <cstdlib>
+#include <thread>
+#include <Windows.h>
 
 using namespace webots;
 
-const std::vector<std::string> vPlayer1Def = {
-	"RED_0", "RED_1", "RED_2", "RED_GOAL"
+const std::vector<std::string> vPlayerDefLeft = {
+	"R0", "R1", "R2", "RG"
 };
 
-const std::vector<std::string> vPlayer2Def = {
-	"BLUE_0", "BLUE_1", "BLUE_2", "BLUE_GOAL"
+const std::vector<std::string> vPlayerDefRight = {
+	"B0", "B1", "B2", "BG"
 };
 
 const std::string ballDef = "Ball";
 
+void _Node::updatePosition()
+{
+	memcpy(&translation, pNode->getField("translation"), sizeof(translation));
+	memcpy(&rotation, pNode->getField("rotation"), sizeof(rotation));
+}
+
+void SoccerReferee::readConfig()
+{
+	
+}
+
 SoccerReferee::SoccerReferee()
 {
-	for (int i = 0; i < vPlayer1Def.size(); i++)
+	for (int i = 0; i < vPlayerDefLeft.size(); i++)
 	{
-		Node* pNode = getFromDef(vPlayer1Def[i]);
-		if (pNode == NULL)
+		_Node _node;
+		_node.pNode = getFromDef(vPlayerDefLeft[i]);
+		if (_node.pNode == NULL)
 		{
-			std::cerr << "No DEF " << vPlayer1Def[i] << "node found in the current world file" << std::endl;
+			std::cerr << "No DEF " << vPlayerDefLeft[i] << "node found in the current world file" << std::endl;
 			exit(1);
 		}
-		vPlayerNodes1.push_back(pNode);
+		vPlayerLeftNodes.push_back(_node);
 	}
 
-	for (int i = 0; i < vPlayer2Def.size(); i++)
+	for (int i = 0; i < vPlayerDefRight.size(); i++)
 	{
-		Node* pNode = getFromDef(vPlayer2Def[i]);
-		if (pNode == NULL)
+		_Node _node;
+		_node.pNode = getFromDef(vPlayerDefRight[i]);
+		if (_node.pNode == NULL)
 		{
-			std::cerr << "No DEF " << vPlayer2Def[i] << "node found in the current world file" << std::endl;
+			std::cerr << "No DEF " << vPlayerDefRight[i] << "node found in the current world file" << std::endl;
 			exit(1);
 		}
-		vPlayerNodes2.push_back(pNode);
+		vPlayerRightNodes.push_back(_node);
 	}
 
-	pBallNode = getFromDef(ballDef);
-	if (pBallNode == NULL)
+	ballNode.pNode = getFromDef(ballDef);
+	if (ballNode.pNode == NULL)
 	{
 		std::cerr << "No DEF " << ballDef << "node found in the current world file" << std::endl;
 		exit(1);
 	}
 
+	pEmitter = getEmitter("emitter");
+	pReceiver = getReceiver("receiver");
+	pReceiver->enable(TIME_STEP);
+	
 }
 
 void SoccerReferee::run()
 {
-	
+	std::thread tReceiver(&threadReceiver);
+	while (step(TIME_STEP) != -1)
+	{
+		readPosition();	
+		autoReferee();
 
+	}
+	tReceiver.join();
+}
 
+void SoccerReferee::readPosition()
+{
+	for (int i = 0; i < vPlayerLeftNodes.size(); i++)
+	{
+		for (int i = 0; i < vPlayerLeftNodes.size(); i++)
+			vPlayerLeftNodes[i].updatePosition();
 
+		for (int i = 0; i < vPlayerRightNodes.size(); i++)
+			vPlayerRightNodes[i].updatePosition();
+
+		ballNode.updatePosition();
+	}
+}
+
+void SoccerReferee::threadReceiver()
+{
+	while (step(TIME_STEP) != -1)
+	{
+		if (pReceiver->getQueueLength() > 0)
+		{
+			char* data = (char*)pReceiver->getData();
+			pReceiver->nextPacket();
+		}
+	}
+}
+
+void SoccerReferee::autoReferee()
+{
+	if (currentState == PLAY_ON)
+	{
+		
+	}
+	else if (currentState == KICK_OFF_LEFT || KICK_OFF_RIGHT)
+	{
+
+	}
+	else if (currentState == THROW_IN_LEFT)
+	{
+
+	}
+	else if (currentState == THROW_IN_RIGHT)
+	{
+
+	}
+	else if (currentState == GOAL_KICK_LEFT)
+	{
+
+	}
+	else if (currentState == GOAL_KICK_RIGHT)
+	{
+
+	}
+	else if (currentState == FREE_KICK_LEFT)
+	{
+
+	}
+	else if (currentState == FREE_KICK_RIGHT)
+	{
+
+	}
+	else if (currentState == PENALTY_KICK_LEFT)
+	{
+
+	}
+	else if (currentState == PENALTY_KICK_RIGHT)
+	{
+
+	}
+	else if (currentState == CORNER_KICK_LEFT)
+	{
+
+	}
+	else if (currentState == CORNER_KICK_RIGHT)
+	{
+
+	}
 
 }
+
+
+
+
+
 
--- controllers/referee_controller/soccerreferee.h
+++ controllers/referee_controller/soccerreferee.h
@@ -1,22 +1,111 @@
+#ifndef SOCCER_REFEREE_H
+#define SOCCER_REFEREE_H
+
 #include <webots/Supervisor.hpp>
+#include <webots/Emitter.hpp>
+#include <webots/Receiver.hpp>
 #include <string>
 #include <vector>
+#include <atomic>
+
+#define TIME_STEP 32
 
 using namespace webots;
 
 namespace webots {
 
+class _Node
+{	
+public:
+	void updatePosition();
+	double translation[3] = {0,0,0};
+	double rotation[3] = {0,0,0};
+	Node* pNode = nullptr;
+};
+
 class SoccerReferee : public Supervisor
 {
+
+//events
+enum {
+	START,
+	OUT_OF_ENDLINE_LEFT,
+	OUT_OF_ENDLINE_RIGHT,
+	OUT_OF_SIDELINE_LEFT,
+	OUT_OF_SIDELINE_RIGHT,
+	OFFSIDE_LEFT,
+	OFFSIDE_RIGHT,
+	GOAL_LEFT,
+	GOAL_RIGHT,
+	HANDBALL_LEFT,
+	HANDBALL_RIGHT,
+	HALF_TIME,
+	TIME_UP,
+	NONE
+};
+
+//play mode
+enum {
+	BEFORE_KICK_OFF,
+	PLAY_ON,
+	GAME_OVER,
+	NONE,
+	KICK_OFF_LEFT,
+	KICK_OFF_RIGHT,
+	THROW_IN_LEFT,
+	THROW_IN_RIGHT,
+	GOAL_KICK_LEFT,
+	GOAL_KICK_RIGHT,
+	FREE_KICK_LEFT,
+	FREE_KICK_RIGHT,
+	PENALTY_KICK_LEFT,
+	PENALTY_KICK_RIGHT,
+	CORNER_KICK_LEFT,
+	CORNER_KICK_RIGHT
+};
+
+
 public:
 	SoccerReferee();
 	void run();
 
-private:
-	std::vector<Node*> vPlayerNodes1;
-	std::vector<Node*> vPlayerNodes2;
-	Node* pBallNode;
+protected:
+	void readConfig();
+	void readPosition();
 
+	/**
+	* Protocal Format
+	* (time (now 93.60)
+	* (GS (t 0.00) (pm BeforeKickOff))
+	* (See
+	*	(B (ccs 0.0 1.0 0.05))
+	*	(P (teamRed) (id 1) (ccs 0.0 0.0 0.5))
+	*	(P (teamBlue) (id 2) (ccs 2.0 1.0 0.5)))
+	*/
+	void threadReceiver();
+	void autoReferee();
+private:
+	std::vector<_Node> vPlayerLeftNodes;
+	std::vector<_Node> vPlayerRightNodes;
+	Emitter* pEmitter;
+	Receiver* pReceiver;
+	_Node ballNode;
+	std::atomic<int> currentState;
+	std::atomic<int> flag;
+	int lastBallKeeper;
+	bool isSocreValid;
+	
+	double pitchVec[2];
+	double penaltyVec[2];
+	double goalAreaVec[2];
+	double goalVec[2];
+	double goalZ;
+	double penaltyMarkX;
+	double penaltyMarkD;
+	double centreD;
 };
 
 }
+
+
+#endif
