fd9a0bcb2cedec38525322b7ae9312214c813fdb
Kaiwen Dong <42641126+KW-DONG@users.noreply.github.com>
2023-02-17 00:50:30 +0000

    Add state machine and basic referee system

--- controllers/referee_controller/soccerreferee.cpp
+++ controllers/referee_controller/soccerreferee.cpp
@@ -1,19 +1,47 @@
 #include "soccerreferee.h"
 #include <cstdlib>
 #include <thread>
-#include <Windows.h>
+#include <ctime>
+#include <cstdlib>
+#include <map>
 
 using namespace webots;
 
-const std::vector<std::string> vPlayerDefLeft = {
+#define BALL_DIAMETER 0.05
+
+const std::vector<std::string> vPlayerLeftDef = {
 	"R0", "R1", "R2", "RG"
 };
 
-const std::vector<std::string> vPlayerDefRight = {
+const std::vector<std::string> vPlayerRightDef = {
 	"B0", "B1", "B2", "BG"
 };
 
 const std::string ballDef = "Ball";
+
+std::map<int, std::string> mPlayMode = {
+	{GM_BEFORE_KICK_OFF, "BeforeKickOff"},
+	{GM_PLAY_ON, "PlayOn"},
+	{GM_GAME_OVER, "GameOver"},
+	{GM_NONE, "None"},
+	{GM_KICK_OFF_LEFT, "KickOffLeft"},
+	{GM_KICK_OFF_RIGHT, "KickOffRight"},
+	{GM_THROW_IN_LEFT, "ThrowInLeft"},
+	{GM_THROW_IN_RIGHT, "ThrowInRight"},
+	{GM_GOAL_KICK_LEFT, "GoalKickLeft"},
+	{GM_GOAL_KICK_RIGHT, "GoalKickRight"},
+	{GM_FREE_KICK_LEFT, "FreeKickLeft"},
+	{GM_FREE_KICK_RIGHT, "FreeKickRight"},
+	{GM_PENALTY_KICK_LEFT, "PenaltyKickLeft"},
+	{GM_PENALTY_KICK_RIGHT, "PenaltyKickRight"},
+	{GM_CORNER_KICK_LEFT, "CornerKickLeft"},
+	{GM_CORNER_KICK_RIGHT, "CornerKickRight"},
+};
+
+std::map<int, std::string> mTeamName = {
+	{_PlayerNode::TEAM_LEFT, "teamLeft"},
+	{_PlayerNode::TEAM_RIGHT, "teamRight"}
+};
 
 void _Node::updatePosition()
 {
@@ -27,30 +55,38 @@ void SoccerReferee::readConfig()
 }
 
 SoccerReferee::SoccerReferee()
-{
-	for (int i = 0; i < vPlayerDefLeft.size(); i++)
+{	
+	for (int i = 0; i < vPlayerLeftDef.size(); i++)
 	{
-		_Node _node;
-		_node.pNode = getFromDef(vPlayerDefLeft[i]);
+		_PlayerNode _node;
+		_node.pNode = getFromDef(vPlayerLeftDef[i]);
 		if (_node.pNode == NULL)
 		{
-			std::cerr << "No DEF " << vPlayerDefLeft[i] << "node found in the current world file" << std::endl;
+			std::cerr << "No DEF " << vPlayerLeftDef[i] << " node found in the current world file" << std::endl;
 			exit(1);
 		}
-		vPlayerLeftNodes.push_back(_node);
+		_node.id = i;
+		_node.role = _PlayerNode::PLAYER;
+		_node.team = _PlayerNode::TEAM_LEFT;
+		vPlayerNodes.push_back(_node);
 	}
+	vPlayerNodes[vPlayerNodes.size() - 1].role = _PlayerNode::GOAL_KEEPER;
 
-	for (int i = 0; i < vPlayerDefRight.size(); i++)
+	for (int i = 0; i < vPlayerRightDef.size(); i++)
 	{
-		_Node _node;
-		_node.pNode = getFromDef(vPlayerDefRight[i]);
+		_PlayerNode _node;
+		_node.pNode = getFromDef(vPlayerRightDef[i]);
 		if (_node.pNode == NULL)
 		{
-			std::cerr << "No DEF " << vPlayerDefRight[i] << "node found in the current world file" << std::endl;
+			std::cerr << "No DEF " << vPlayerRightDef[i] << " node found in the current world file" << std::endl;
 			exit(1);
 		}
-		vPlayerRightNodes.push_back(_node);
+		_node.id = i;
+		_node.role = _PlayerNode::PLAYER;
+		_node.team = _PlayerNode::TEAM_RIGHT;
+		vPlayerNodes.push_back(_node);
 	}
+	vPlayerNodes[vPlayerNodes.size() - 1].role = _PlayerNode::GOAL_KEEPER;
 
 	ballNode.pNode = getFromDef(ballDef);
 	if (ballNode.pNode == NULL)
@@ -62,102 +98,440 @@ SoccerReferee::SoccerReferee()
 	pEmitter = getEmitter("emitter");
 	pReceiver = getReceiver("receiver");
 	pReceiver->enable(TIME_STEP);
-	
 }
 
 void SoccerReferee::run()
 {
-	std::thread tReceiver(&threadReceiver);
 	while (step(TIME_STEP) != -1)
 	{
+		readReceiver();
 		readPosition();	
-		autoReferee();
+		if (flag != FLG_HANDBALL_LEFT && flag != FLG_HANDBALL_RIGHT)
+			localReferee();
+		lastGameMode = gameMode;
+		stateDriver();
 
+		//boardcast playmode
+		if (lastGameMode != gameMode)
+		{
+			std::string msg = "(GS (time " + gameTime + ") (pm " + mPlayMode[gameMode] + "))";
+			pEmitter->setChannel(-1);
+			pEmitter->send(msg.c_str(), msg.size());
+		}
+
+		//send position
+		std::string msg = "(See " + seeBall(msg);
+		for (int i = 0; i < vPlayerNodes.size(); i++)
+		{
+			msg = seePlayer(msg, i);
+		}
+		msg += ")";
+		pEmitter->setChannel(-1);
+		pEmitter->send(msg.c_str(), msg.size());
 	}
-	tReceiver.join();
+}
+
+void SoccerReferee::findNodeSection(_Node& node)
+{
+	if (node.translation[0] >= penaltyVec[0] &&
+		node.translation[1] <= penaltyVec[1] &&
+		node.translation[1] >= -penaltyVec[1])
+	{
+		if (node.translation[0] >= goalVec[0] &&
+			node.translation[1] <= goalVec[1] &&
+			node.translation[1] >= -goalVec[1])
+			node.section = PS_LEFT_GOAL_AREA;
+		else node.section = PS_LEFT_PENALTY_AREA;
+	}
+	else if (node.translation[0] <= -penaltyVec[0] &&
+		node.translation[1] <= penaltyVec[1] &&
+		node.translation[1] >= -penaltyVec[1])
+	{
+		if (node.translation[0] <= -goalVec[0] &&
+			node.translation[1] <= goalVec[1] &&
+			node.translation[1] >= -goalVec[1])
+			node.section = PS_RIGHT_GOAL_AREA;
+		else node.section = PS_RIGHT_PENALTY_AREA;
+	}
+	else if (node.translation[0] < 0)
+		node.section = PS_LEFT_NORMAL;
+	else
+		node.section = PS_RIGHT_NORMAL;
+}
+
+void SoccerReferee::findBallSection()
+{
+	if (ballNode.translation[0] < -pitchVec[0] && ballNode.translation[0] > -goalVec[0] &&
+		ballNode.translation[1] < goalVec[1] && ballNode.translation[1] > -goalVec[1] &&
+		ballNode.translation[2] < goalZ)
+		ballNode.section = PS_LEFT_GOAL;
+	else if (ballNode.translation[0] < pitchVec[0] && ballNode.translation[0] < goalVec[0] &&
+		ballNode.translation[1] < goalVec[1] && ballNode.translation[1] > -goalVec[1] &&
+		ballNode.translation[2] < goalZ)
+		ballNode.section = PS_LEFT_GOAL;
+	//ball hit ground
+	else if (ballNode.translation[2] <= BALL_DIAMETER)
+	{
+		if (ballNode.translation[0] < -pitchVec[0])
+			ballNode.section = PS_OUT_LEFT_ENDLINE;
+		else if (ballNode.translation[0] > pitchVec[0])
+			ballNode.section = PS_OUT_RIGHT_ENDLINE;
+		else if (ballNode.translation[1] < -pitchVec[1] || ballNode.translation[1] > pitchVec[1])
+			ballNode.section = PS_OUT_SIDELINE;
+	}
+	else
+	{
+		findNodeSection(ballNode);
+	}
 }
 
 void SoccerReferee::readPosition()
 {
-	for (int i = 0; i < vPlayerLeftNodes.size(); i++)
+	for (int i = 0; i < vPlayerNodes.size(); i++)
 	{
-		for (int i = 0; i < vPlayerLeftNodes.size(); i++)
-			vPlayerLeftNodes[i].updatePosition();
+		vPlayerNodes[i].updatePosition();
+		findNodeSection(vPlayerNodes[i]);
+	}
+	ballNode.updatePosition();
+}
 
-		for (int i = 0; i < vPlayerRightNodes.size(); i++)
-			vPlayerRightNodes[i].updatePosition();
+void SoccerReferee::readReceiver()
+{
+	if (pReceiver->getQueueLength() > 0)
+	{
+		std::string data = (char*)pReceiver->getData();
+		std::string sHeader, sTeam, sId;
+		int offset = readHeader(data, sHeader, 0);
+		offset = readProperty(data, sTeam, offset);
+		readProperty(data, sId, offset);
+		lastBallKeeperId = atoi(sId.c_str());
 
-		ballNode.updatePosition();
+		if (sTeam == "left")
+		{
+			lastBallKeeperPosition[0] = vPlayerNodes[lastBallKeeperId].translation[0];
+			lastBallKeeperPosition[1] = vPlayerNodes[lastBallKeeperId].translation[1];
+			lastBallKeeperRole = vPlayerNodes[lastBallKeeperId].role;
+			lastBallKeeperTeam = _PlayerNode::TEAM_LEFT;
+		}		
+		else if (sTeam == "right")
+		{
+			lastBallKeeperPosition[0] = vPlayerNodes[lastBallKeeperId + 4].translation[0];
+			lastBallKeeperPosition[1] = vPlayerNodes[lastBallKeeperId + 4].translation[1];
+			lastBallKeeperRole = vPlayerNodes[lastBallKeeperId + 4].role;
+			lastBallKeeperTeam = _PlayerNode::TEAM_RIGHT;
+		}
+		
+		if (sHeader == "HB")
+		{
+			if (lastBallKeeperTeam == _PlayerNode::TEAM_LEFT)
+			{
+				if (lastBallKeeperRole == _PlayerNode::GOAL_KEEPER)
+					flag = FLG_KICKBALL_LEFT;
+				else
+					flag = FLG_HANDBALL_LEFT;
+			}
+			else if (sTeam == "RIGHT")
+			{
+				if (lastBallKeeperRole == _PlayerNode::GOAL_KEEPER)
+					flag = FLG_KICKBALL_RIGHT;
+				else
+					flag = FLG_HANDBALL_RIGHT;
+			}
+		}
+		else if (sHeader == "KB")
+		{
+			if (sTeam == "LEFT")
+				flag = FLG_KICKBALL_LEFT;
+			else
+				flag = FLG_KICKBALL_RIGHT;
+		}
+		pReceiver->nextPacket();
 	}
 }
 
-void SoccerReferee::threadReceiver()
-{
-	while (step(TIME_STEP) != -1)
+void SoccerReferee::localReferee()
+{		
+	if (gameMode == GM_PLAY_ON)
 	{
-		if (pReceiver->getQueueLength() > 0)
+		if (ballNode.section == PS_LEFT_GOAL)
+			flag = FLG_GOAL_RIGHT;
+		else if (ballNode.section == PS_RIGHT_GOAL)
+			flag = FLG_GOAL_LEFT;
+		else if (ballNode.section == PS_OUT_SIDELINE)
 		{
-			char* data = (char*)pReceiver->getData();
-			pReceiver->nextPacket();
+			if (lastBallKeeperTeam == _PlayerNode::TEAM_LEFT)
+				flag = FLG_OUT_OF_SIDELINE_LEFT;
+			else
+				flag = FLG_OUT_OF_SIDELINE_RIGHT;
+		}
+		else if (ballNode.section == PS_OUT_LEFT_ENDLINE)
+			flag = FLG_OUT_OF_ENDLINE_LEFT;
+		else if (ballNode.section == PS_OUT_RIGHT_ENDLINE)
+			flag = FLG_OUT_OF_ENDLINE_RIGHT;
+		//offside
+		else if (ballNode.section > PS_LEFT_GOAL || ballNode.section <= PS_LEFT_NORMAL)
+		{
+			
+		}
+		else if (ballNode.section > PS_RIGHT_GOAL || ballNode.section <= PS_RIGHT_NORMAL)
+		{
+
 		}
 	}
+	else if (gameMode == GM_KICK_OFF_LEFT)
+	{
+		if (flag == FLG_KICKBALL_LEFT)
+			flag = FLG_COMPLETE;
+	}
+	else if (gameMode == GM_KICK_OFF_RIGHT)
+	{
+		if (flag == FLG_KICKBALL_RIGHT)
+			flag = FLG_COMPLETE;
+	}
+	else if (gameMode == GM_THROW_IN_LEFT)
+	{
+		if (flag == FLG_KICKBALL_LEFT)
+			flag = FLG_COMPLETE;
+	}
+	else if (gameMode == GM_THROW_IN_RIGHT)
+	{
+		if (flag == FLG_KICKBALL_RIGHT)
+			flag = FLG_COMPLETE;
+	}
+	else if (gameMode == GM_GOAL_KICK_LEFT)
+	{
+		if (flag == FLG_KICKBALL_LEFT)
+			flag = FLG_COMPLETE;
+	}
+	else if (gameMode == GM_GOAL_KICK_RIGHT)
+	{
+		if (flag == FLG_KICKBALL_RIGHT)
+			flag = FLG_COMPLETE;
+	}
+	else if (gameMode == GM_FREE_KICK_LEFT)
+	{
+		if (flag == FLG_KICKBALL_LEFT)
+			flag = FLG_COMPLETE;
+	}
+	else if (gameMode == GM_FREE_KICK_RIGHT)
+	{
+		if (flag == FLG_KICKBALL_RIGHT)
+			flag = FLG_COMPLETE;
+	}
+	else if (gameMode == GM_PENALTY_KICK_LEFT)
+	{
+		if (flag == FLG_KICKBALL_LEFT)
+			flag = FLG_COMPLETE;
+	}
+	else if (gameMode == GM_PENALTY_KICK_RIGHT)
+	{
+		if (flag == FLG_KICKBALL_RIGHT)
+			flag = FLG_COMPLETE;
+	}
+	else if (gameMode == GM_CORNER_KICK_LEFT)
+	{
+		if (flag == FLG_KICKBALL_LEFT)
+			flag = FLG_COMPLETE;
+	}
+	else if (gameMode == GM_CORNER_KICK_RIGHT)
+	{
+		if (flag == FLG_KICKBALL_RIGHT)
+			flag = FLG_COMPLETE;
+	}
 }
 
-void SoccerReferee::autoReferee()
+void SoccerReferee::stateDriver()
 {
-	if (currentState == PLAY_ON)
+	switch (flag)
 	{
-		
+	case FLG_START:
+		onStart();
+		break;
+	case FLG_OUT_OF_ENDLINE_LEFT:
+		onOutOfEndLineLeft();
+		break;
+	case FLG_OUT_OF_ENDLINE_RIGHT:
+		onOutOfEndLineRight();
+		break;
+	case FLG_OUT_OF_SIDELINE_LEFT:
+		onOutOfSideLineLeft();
+		break;
+	case FLG_OUT_OF_SIDELINE_RIGHT:
+		onOutOfSideLineRight();
+		break;
+	case FLG_OFFSIDE_LEFT:
+		onOffsideLeft();
+		break;
+	case FLG_OFFSIDE_RIGHT:
+		onOffsideRight();
+		break;
+	case FLG_GOAL_LEFT:
+		onGoalLeft();
+		break;
+	case FLG_GOAL_RIGHT:
+		onGoalRight();
+		break;
+	case FLG_HANDBALL_LEFT:
+		onHandballLeft();
+		break;
+	case FLG_HANDBALL_RIGHT:
+		onHandballRight();
+		break;
+	case FLG_TIME_UP:
+		onTimeUp();
+		break;
 	}
-	else if (currentState == KICK_OFF_LEFT || KICK_OFF_RIGHT)
-	{
-
-	}
-	else if (currentState == THROW_IN_LEFT)
-	{
-
-	}
-	else if (currentState == THROW_IN_RIGHT)
-	{
-
-	}
-	else if (currentState == GOAL_KICK_LEFT)
-	{
-
-	}
-	else if (currentState == GOAL_KICK_RIGHT)
-	{
-
-	}
-	else if (currentState == FREE_KICK_LEFT)
-	{
-
-	}
-	else if (currentState == FREE_KICK_RIGHT)
-	{
-
-	}
-	else if (currentState == PENALTY_KICK_LEFT)
-	{
-
-	}
-	else if (currentState == PENALTY_KICK_RIGHT)
-	{
-
-	}
-	else if (currentState == CORNER_KICK_LEFT)
-	{
-
-	}
-	else if (currentState == CORNER_KICK_RIGHT)
-	{
-
-	}
-
 }
 
+bool SoccerReferee::isBallHitGround()
+{
+	if (ballNode.translation[2] <= BALL_DIAMETER)	return true;
+	return false;
+}
 
+void SoccerReferee::onStart()
+{
+	//start game timer
 
+	//update display
+
+	
+	gameMode = GM_BEFORE_KICK_OFF;
+}
+
+void SoccerReferee::onOutOfEndLineLeft()
+{
+	if (lastBallKeeperTeam == _PlayerNode::TEAM_LEFT)
+		gameMode = GM_CORNER_KICK_LEFT;
+	else
+		gameMode = GM_GOAL_KICK_LEFT;
+	flag = FLG_NONE;
+}
+
+void SoccerReferee::onOutOfEndLineRight()
+{
+	if (lastBallKeeperTeam == _PlayerNode::TEAM_LEFT)
+		gameMode = GM_GOAL_KICK_RIGHT;
+	else
+		gameMode = GM_CORNER_KICK_RIGHT;
+	flag = FLG_NONE;
+}
+
+void SoccerReferee::onOutOfSideLineLeft()
+{
+	
+	gameMode = GM_THROW_IN_RIGHT;
+	flag = FLG_NONE;
+}
+
+void SoccerReferee::onOutOfSideLineRight()
+{
+
+	gameMode = GM_THROW_IN_LEFT;
+	flag = FLG_NONE;
+}
+
+void SoccerReferee::onOffsideLeft()
+{
+	flag = FLG_NONE;
+}
+
+void SoccerReferee::onOffsideRight()
+{
+	flag = FLG_NONE;
+}
+
+void SoccerReferee::onGoalLeft()
+{
+	score[0]++;
+	gameMode = GM_KICK_OFF_RIGHT;
+	flag = FLG_NONE;
+}
+
+void SoccerReferee::onGoalRight()
+{
+	score[1]++;
+	gameMode = GM_KICK_OFF_LEFT;
+	flag = FLG_NONE;
+}
+
+void SoccerReferee::onHandballLeft()
+{
+	flag = FLG_NONE;
+}
+
+void SoccerReferee::onHandballRight()
+{
+	flag = FLG_NONE;
+}
+
+void SoccerReferee::onTimeUp()
+{
+	gameMode = GM_GAME_OVER;
+	flag = FLG_NONE;
+}
+
+void SoccerReferee::onComplete()
+{
+	gameMode = GM_PLAY_ON;
+	flag = FLG_NONE;
+}
+
+int SoccerReferee::readHeader(std::string& src, std::string& dst, int offset=0)
+{
+	int start = 0;
+	int end = 0;
+	for (int i = offset; i < src.size(); i++)
+	{
+		if (src[i] == '(')
+			start = i;
+		else if (src[i] == ' ')
+		{
+			end = i;
+			break;
+		}
+	}
+	dst = src.substr(start, end - start);
+	return end;
+}
+
+int SoccerReferee::readProperty(std::string& src, std::string& dst, int offset)
+{
+	int start = 0;
+	int end = 0;
+
+	for (int i = offset; i < src.size(); i++)
+	{
+		if (src[i] == '<')
+			start = i;
+		else if (src[i] == '>')
+		{
+			end = i;
+			break;
+		}
+	}
+	dst = src.substr(start, end - start);
+	return end;
+}
+
+std::string SoccerReferee::seeBall(std::string& src)
+{
+	std::string msg = src + "(B (ccs " + 
+		std::to_string(ballNode.translation[0]) + " " +
+		std::to_string(ballNode.translation[1]) + " " +
+		std::to_string(ballNode.translation[2]) + "))";
+	return msg;
+}
+
+std::string SoccerReferee::seePlayer(std::string& src, int index)
+{
+	std::string msg = src + "(P (team " + mTeamName[vPlayerNodes[index].team] + ") (id " + 
+		std::to_string(vPlayerNodes[index].id) + " ) (ccs " + 
+		std::to_string(vPlayerNodes[index].translation[0]) + " " +
+		std::to_string(vPlayerNodes[index].translation[1]) + " " +
+		std::to_string(vPlayerNodes[index].translation[2]) + "))";
+	return msg;
+}
 
 
 
--- controllers/referee_controller/soccerreferee.h
+++ controllers/referee_controller/soccerreferee.h
@@ -8,11 +8,33 @@
 #include <vector>
 #include <atomic>
 
-#define TIME_STEP 32
-
 using namespace webots;
 
 namespace webots {
+
+#define TIME_STEP 32
+#define IS_LEFT_SEC(x) (x < 0)
+#define IS_RIGHT_SEC(x) (x > 0)
+
+//play mode
+enum {
+	GM_BEFORE_KICK_OFF,
+	GM_PLAY_ON,
+	GM_GAME_OVER,
+	GM_NONE,
+	GM_KICK_OFF_LEFT,
+	GM_KICK_OFF_RIGHT,
+	GM_THROW_IN_LEFT,
+	GM_THROW_IN_RIGHT,
+	GM_GOAL_KICK_LEFT,
+	GM_GOAL_KICK_RIGHT,
+	GM_FREE_KICK_LEFT,
+	GM_FREE_KICK_RIGHT,
+	GM_PENALTY_KICK_LEFT,
+	GM_PENALTY_KICK_RIGHT,
+	GM_CORNER_KICK_LEFT,
+	GM_CORNER_KICK_RIGHT
+};
 
 class _Node
 {	
@@ -21,80 +43,147 @@ public:
 	double translation[3] = {0,0,0};
 	double rotation[3] = {0,0,0};
 	Node* pNode = nullptr;
+	int section;
+};
+
+class _PlayerNode : public _Node
+{
+public:
+	enum {
+		TEAM_LEFT,
+		TEAM_RIGHT
+	};
+	int team;
+	enum {
+		PLAYER,
+		GOAL_KEEPER
+	};
+	int role;
+	int id;
 };
 
 class SoccerReferee : public Supervisor
 {
+//player id
+enum {
+	PLAYER_LEFT_0,
+	PLAYER_LEFT_1,
+	PLAYER_LEFT_2,
+	GOAL_KEEPER_LEFT,
+	PLAYER_RIGHT_0,
+	PLAYER_RIGHT_1,
+	PLAYER_RIGHT_2,
+	GOAL_KEEPER_RIGHT
+};
 
 //events
 enum {
-	START,
-	OUT_OF_ENDLINE_LEFT,
-	OUT_OF_ENDLINE_RIGHT,
-	OUT_OF_SIDELINE_LEFT,
-	OUT_OF_SIDELINE_RIGHT,
-	OFFSIDE_LEFT,
-	OFFSIDE_RIGHT,
-	GOAL_LEFT,
-	GOAL_RIGHT,
-	HANDBALL_LEFT,
-	HANDBALL_RIGHT,
-	HALF_TIME,
-	TIME_UP,
-	NONE
+	FLG_START,
+	FLG_OUT_OF_ENDLINE_LEFT,
+	FLG_OUT_OF_ENDLINE_RIGHT,
+	FLG_OUT_OF_SIDELINE_LEFT,
+	FLG_OUT_OF_SIDELINE_RIGHT,
+	FLG_OFFSIDE_LEFT,
+	FLG_OFFSIDE_RIGHT,
+	FLG_GOAL_LEFT,
+	FLG_GOAL_RIGHT,
+	FLG_HANDBALL_LEFT,
+	FLG_HANDBALL_RIGHT,
+	FLG_KICKBALL_LEFT,
+	FLG_KICKBALL_RIGHT,
+	FLG_BALL_HIT_GROUND,
+	FLG_HALF_TIME,
+	FLG_TIME_UP,
+	FLG_NONE,
+	FLG_COMPLETE
 };
 
-//play mode
+
+
+//Pitch section
 enum {
-	BEFORE_KICK_OFF,
-	PLAY_ON,
-	GAME_OVER,
-	NONE,
-	KICK_OFF_LEFT,
-	KICK_OFF_RIGHT,
-	THROW_IN_LEFT,
-	THROW_IN_RIGHT,
-	GOAL_KICK_LEFT,
-	GOAL_KICK_RIGHT,
-	FREE_KICK_LEFT,
-	FREE_KICK_RIGHT,
-	PENALTY_KICK_LEFT,
-	PENALTY_KICK_RIGHT,
-	CORNER_KICK_LEFT,
-	CORNER_KICK_RIGHT
+	PS_LEFT_GOAL = -4,
+	PS_LEFT_GOAL_AREA,
+	PS_LEFT_PENALTY_AREA,
+	PS_LEFT_NORMAL,
+	PS_OUT_LEFT_ENDLINE,
+	PS_OUT_RIGHT_ENDLINE,
+	PS_OUT_SIDELINE,
+	PS_RIGHT_GOAL,
+	PS_RIGHT_GOAL_AREA,
+	PS_RIGHT_PENALTY_AREA,
+	PS_RIGHT_NORMAL,
 };
-
 
 public:
 	SoccerReferee();
 	void run();
+	void findNodeSection(_Node& node);
+	void findBallSection();
 
 protected:
 	void readConfig();
 	void readPosition();
 
 	/**
-	* Protocal Format
-	* (time (now 93.60)
-	* (GS (t 0.00) (pm BeforeKickOff))
+	* Protocal
+	* (time (now 93.60)	//system time
+	* (GS (t 0.00) (pm BeforeKickOff))	//game time and game mode
 	* (See
-	*	(B (ccs 0.0 1.0 0.05))
-	*	(P (teamRed) (id 1) (ccs 0.0 0.0 0.5))
+	*	(B (ccs 0.0 1.0 0.05))	//ball
+	*	(P (teamRed) (id 1) (ccs 0.0 0.0 0.5))	//player
 	*	(P (teamBlue) (id 2) (ccs 2.0 1.0 0.5)))
 	*/
-	void threadReceiver();
-	void autoReferee();
+	void readReceiver();
+	void localReferee();
+	void stateDriver();
+	bool isBallHitGround();
+
+protected:
+	//callbacks
+	void onStart();
+	void onOutOfEndLineLeft();
+	void onOutOfEndLineRight();
+	void onOutOfSideLineLeft();
+	void onOutOfSideLineRight();
+	void onOffsideLeft();
+	void onOffsideRight();
+	void onGoalLeft();
+	void onGoalRight();
+	void onHandballLeft();
+	void onHandballRight();	
+	void onTimeUp();
+	void onComplete();
+
+	int readHeader(std::string& src, std::string& dst, int offset=0);
+	int readProperty(std::string& src, std::string& dst, int offset);
+
+	std::string seeBall(std::string& src);
+	std::string seePlayer(std::string& src, int id);
+
 private:
-	std::vector<_Node> vPlayerLeftNodes;
-	std::vector<_Node> vPlayerRightNodes;
+	std::vector<_PlayerNode> vPlayerNodes;
+	//std::vector<_Node> vPlayerLeftNodes;
+	//std::vector<_Node> vPlayerRightNodes;
 	Emitter* pEmitter;
 	Receiver* pReceiver;
 	_Node ballNode;
-	std::atomic<int> currentState;
-	std::atomic<int> flag;
-	int lastBallKeeper;
-	bool isSocreValid;
+	int gameMode;
+	int lastGameMode;
+	int flag;
+
+	//last ball keeper detail
+
+	int lastBallKeeperTeam;
+	int lastBallKeeperRole;
+	int lastBallKeeperId;
+	double lastBallKeeperPosition[2];
+
+	//time
+	std::string gameTime;
+	double systemTime;
 	
+	//pitch detail
 	double pitchVec[2];
 	double penaltyVec[2];
 	double goalAreaVec[2];
@@ -103,6 +192,9 @@ private:
 	double penaltyMarkX;
 	double penaltyMarkD;
 	double centreD;
+
+	int score[2] = {0,0};
+
 };
 
 }
