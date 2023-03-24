f1d77c75240b182387ac08366ab45a287b12418a
Kaiwen Dong <42641126+KW-DONG@users.noreply.github.com>
2023-03-24 13:00:36 +0000

    Create referee_controller.cpp

---
+++ controllers/referee_controller/referee_controller.cpp
@@ -0,0 +1,32 @@
+// File:          referee_controller.cpp
+// Date:
+// Description:
+// Author:
+// Modifications:
+
+// You may need to add webots include files such as
+// <webots/DistanceSensor.hpp>, <webots/Motor.hpp>, etc.
+// and/or to add some other includes
+#include <webots/Robot.hpp>
+#include <iostream>
+#include "soccerreferee.h"
+
+// All the webots classes are defined in the "webots" namespace
+using namespace webots;
+
+// This is the main program of your controller.
+// It creates an instance of your Robot instance, launches its
+// function(s) and destroys it at the end of the execution.
+// Note that only one instance of Robot should be created in
+// a controller program.
+// The arguments of the main function can be specified by the
+// "controllerArgs" field of the Robot node
+int main(int argc, char **argv) {
+
+	SoccerReferee *robot = new SoccerReferee();
+
+	robot->run();
+
+	delete robot;
+	return 0;
+}
